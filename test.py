145040678e43d871db3d5b6392c8a6036364719e
Oluwatosin Olubiyi <olubiyiontheweb@gmail.com>
2022-08-06 21:03:06 +0100

    web scrapping tool completed

--- test.py
+++
@@ -1,8 +0,0 @@
-g = {}
-a = {'test.txt': 'tosin', 'test2.txt': 'gabriel', 'test3.txt': 'tosin'}
-x = lambda b: a[b]
-for i in a:	
-    if x(i) in g:
-    	g[x(i)].append(i)
-    else:
-    	g[x(i)] = [i]
